5fb3eae77167b233eaadba33a1c7a02f5503a6b9
Arkes123 <jonathan.darius2015@gmail.com>
2023-05-16 12:32:13 -0700

    added item.h

--- header/item.h
+++ header/item.h
@@ -1,4 +1,29 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+class Items {
+    protected:
+        string itemName_;
+        string itemTime_;
+        string itemLocation_;
+        string itemDescrip_;
+        int itemPriority_;
+        string itemType_;
+        bool itemCompletion;
+        
+    public:
+
+        string getName();
+        string getTime();
+        string getLocation();
+        string getDescription();
+        int getPriority();
+        string getItemType();
+        bool getStatus();
+        Items();
+        Items(string, string, string, string, int, string, bool);
+        virtual void printItem() = 0;
+
+}
+
 #endif
