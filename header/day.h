dc258d7b9869b8b3f1b9238f9fbb62a2935b2ceb
Jeli04 <jli793@ucr.edu>
2023-05-24 18:20:10 -0700

    Added get dayName with ctime

--- header/day.h
+++ header/day.h
@@ -1,4 +1,17 @@
 #ifndef DAY_H
 #define DAY_H
 
+#include "../header/toDoList.h"
+#include <ctime>
+
+class Day : public ToDoList{
+private:
+    string dayName;
+    int dayNumber;
+    list<Item*> listOfItems;
+public:
+    Day(string date); 
+
+};
+
 #endif
