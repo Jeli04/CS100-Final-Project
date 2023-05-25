79a7a5e7796f744858c9a5e760da049b9ed945b6
Jeli04 <jli793@ucr.edu>
2023-05-24 18:35:11 -0700

    removed accidental day cpp code

--- src/day.cpp
+++ src/day.cpp
@@ -1,16 +1,1 @@
 #include "../header/day.h"
-
-Day::Day(string date){
-    dayNumber = stoi(date.substr(0, 2));  // gets the first two characters
-
-    // gets the day of the week
-    tm time_in = {0, 0, 0, 24, 10, 2023-2020};
-    time_t time_temp = mktime(&time_in);
-    dayName = localtime(&time_temp)->tm_wday;   // gets the day name
-   
-    for(Item* listItem : AllItems){
-        if(listItem->)
-        // if listItem->date == date
-        // add to listOfItems
-    }
-}
