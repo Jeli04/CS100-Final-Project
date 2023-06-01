33ad6b8fc975ac6d6fdc4ca5ec29997eb09b8b28
Jeli04 <jli793@ucr.edu>
2023-06-01 12:35:41 -0700

    Added executable for main menu

--- header/mainMenu.h
+++ header/mainMenu.h
@@ -1,4 +1,27 @@
 #ifndef MAINMENU_H
 #define MAINMENU_H
 
+#include <iostream>
+#include "courseList.h"
+#include "calendar.h"
+#include "toDoList.h"
+
+using namespace std;
+
+class MainMenu{
+private:   
+    // ToDoList* toDoList;
+    // CourseList* courseList;
+    // Calendar* calendar;
+    char previousPrompt;
+    char currentPrompt;
+
+    void back(const char prev, const char curr);
+public:
+    void homePrompt(ostream& ss);
+    void calendarPrompt(ostream& ss);
+    void courseListPrompt(ostream& ss);
+    void toDoLisetPrompt(ostream& ss);
+};
+
 #endif
--- src/mainMenu.cpp
+++ src/mainMenu.cpp
@@ -1,1 +1,16 @@
 #include "../header/mainMenu.h"
+
+void MainMenu::homePrompt(ostream& ss){
+    ss << "Welcome to Priority Flow" << endl;
+    ss << "Please enter 'T' to view your To-Do-List" << endl;
+    ss << "Please enter 'S' to view your Course List" << endl;
+    ss << "Please enter 'C' to view your Calendar" << endl;
+}
+
+
+int main(){
+    MainMenu mainMenu;
+    mainMenu.homePrompt(cout);
+
+    return 0;
+}
