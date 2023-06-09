aab94079e1db2830f175c9833b22cc4ff314ad74
RiyanaD <riyana.duttaa@gmail.com>
2023-06-09 01:00:38 -0700

    stashing to checkout rdutt010/fixingInputVal branch

--- tests/googletest/googlemock/CTestTestfile.cmake
+++ tests/googletest/googlemock/CTestTestfile.cmake
@@ -1,7 +1,6 @@
 # CMake generated Testfile for 
-
-# Source directory: /home/csmajs/jli793/final-project-sghos026-jli793-rdutt010-jdari003/tests/googletest/googlemock
-# Build directory: /home/csmajs/jli793/final-project-sghos026-jli793-rdutt010-jdari003/tests/googletest/googlemock
+# Source directory: /home/csmajs/rdutt010/github-classroom/cs100/final-project-sghos026-jli793-rdutt010-jdari003/tests/googletest/googlemock
+# Build directory: /data/home/csmajs/rdutt010/github-classroom/cs100/final-project-sghos026-jli793-rdutt010-jdari003/tests/googletest/googlemock
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
